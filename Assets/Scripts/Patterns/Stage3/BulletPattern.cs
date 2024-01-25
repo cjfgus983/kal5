15d2e0dbee790d10b1e53278a4f6e4f2e4a6a464
inganjo <hysoo2576@gmail.com>
2024-01-25 20:45:35 +0900

    1/23

---
+++ Assets/Scripts/Patterns/Stage3/BulletPattern.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletPattern : PatternData
+{
+    public Vector2 bulletDirect;
+    public float bulletSpeed;
+    public GameObject player;
+    public bool aimPlayer;
+    public bool isShotGun;
+    Rigidbody2D rigid;
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        base.Start();
+        rigid = GetComponent<Rigidbody2D>();
+        player = GameObject.Find("Player");
+        if (isShotGun)
+            ShotgunAimtoPlayer();
+        else if (aimPlayer)
+            AimtoPlayer();
+        else
+            AimDecision();
+    }
+
+    // Update is called once per frame
+    protected override void Update()
+    {
+        base.Update();
+        if (aimPlayer)
+            ShotgunAimtoPlayer();
+    }
+    void ShotgunAimtoPlayer()
+    {
+        transform.LookAt(player.transform);
+    }
+
+    void AimtoPlayer()
+    {
+        Vector2 dir = (player.transform.position - transform.position).normalized;
+        rigid.AddForce(dir*bulletSpeed, ForceMode2D.Impulse);
+    }
+
+    void AimDecision()
+    {
+        rigid.AddForce(Vector2.right * bulletSpeed*bulletDirect.x, ForceMode2D.Impulse);
+        rigid.AddForce(Vector2.up * bulletSpeed* bulletDirect.y, ForceMode2D.Impulse);
+    }
+}
