c0a7d42b8d14fc07a2ec268e746b41f81d4c472c
Roco0309 <69726673+Roco0309@users.noreply.github.com>
2024-03-12 21:36:57 +0900

    changedprogressbar

--- Assets/Scripts/ProgressBar.cs
+++ Assets/Scripts/ProgressBar.cs
@@ -8,20 +8,14 @@ public class ProgressBar : MonoBehaviour
     public AudioSource musicSource;
     public Slider progressBar;
 
-    public GameObject clearUI;
-
     private void Update()
     {
-        progressBar.value = musicSource.time / musicSource.clip.length;
-        if(progressBar.value == 1)
-		{
-
-            StartCoroutine(ClearUIOpen());
+        if (musicSource.isPlaying)
+        {
+            progressBar.value = musicSource.time / musicSource.clip.length;
+        } else if (musicSource.enabled == true && musicSource.isPlaying == false)
+        {
+            progressBar.value = 1;
         }
-    }
-    IEnumerator ClearUIOpen()
-	{
-        yield return new WaitForSeconds(1f);
-        clearUI.SetActive(true);
     }
 }
